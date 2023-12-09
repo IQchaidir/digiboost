d41656ac598dc477d5cc15ddcfb6bfdc5304dcc8
Iqbal Chaidir <iqbal.chaidir@gmail.com>
2023-12-09 11:46:39 +0700

    hero smartphone

--- src/components/mainHero.js
+++ src/components/mainHero.js
@@ -8,7 +8,8 @@ import { RiServiceLine } from "react-icons/ri";
 const MainHero = () => {
   return (
     <>
-      <div className="flex pt-40 justify-center h-auto px-40 ">
+      {/* Tampilan Desktop */}
+      <div className="hidden sm:flex pt-40 justify-center h-auto px-40">
         <div className="text-black text-left w-1/2 pt-7">
           <div className="bg-black p-2 inline-block">
             <h2 className="text-4xl font-extrabold text-white">BOOST...!</h2>
@@ -38,7 +39,7 @@ const MainHero = () => {
           />
         </div>
       </div>
-      <div className="flex justify-center mt-[-40px] ml-96">
+      <div className="hidden sm:flex lg:flex justify-center mt-[-40px] ml-96">
         <div className=" flex justify-between items-center bg-black w-auto gap-9 h-auto rounded-xl px-12 py-6">
           <div className="flex flex-col justify-center items-center text-white">
             <div className="w-auto h-auto bg-white rounded-full flex items-center justify-center p-2">
@@ -64,6 +65,68 @@ const MainHero = () => {
             </div>
 
             <p className="text-xl font-bold pt-6">Trusted Service</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Tampilan Smartphone */}
+      <div className="flex flex-col items-center pt-32 px-4 sm:hidden">
+        <div className="text-black text-center">
+          <div className="bg-black p-2 inline-block">
+            <h2 className="text-2xl font-extrabold text-white">BOOST...!</h2>
+          </div>
+          <h1 className="text-3xl font-extrabold pt-4">We Help You Grow</h1>
+          <h1 className="text-3xl font-extrabold pt-2">Your Business</h1>
+          <p className="text-md pt-4">
+            Place your trust in us, and we'll provide significant growth for
+            your business. Like other great agencies, we are committed to
+            delivering only the best results.
+          </p>
+          <div className="pt-4">
+            <button className="bg-red-400 text-sm rounded-lg text-white px-4 py-2">
+              Our Service
+            </button>
+          </div>
+        </div>
+        <div className="mt-8">
+          <Image
+            src="/heroimg.webp"
+            alt="Hero Image"
+            width={300}
+            height={300}
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
+
+      {/* Tampilan Smartphone - Icon Services */}
+      <div className="flex flex-col items-center sm:hidden">
+        <div className="bg-black w-full rounded-xl px-4 py-2 ">
+          <div className="flex flex-wrap justify-center gap-4">
+            <div className="flex flex-col items-center text-white">
+              <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center p-2">
+                <CiClock1 className="w-6 h-6 text-black" />
+              </div>
+              <p className="text-xs font-bold pt-2">24/7 Availability</p>
+            </div>
+            <div className="flex flex-col items-center text-white">
+              <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center p-2">
+                <AiOutlineThunderbolt className="w-6 h-6 text-black" />
+              </div>
+              <p className="text-xs font-bold pt-2">Quick Response</p>
+            </div>
+            <div className="flex flex-col items-center text-white">
+              <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center p-2">
+                <IoPricetagsOutline className="w-6 h-6 text-black" />
+              </div>
+              <p className="text-xs font-bold pt-2">Price Affordable</p>
+            </div>
+            <div className="flex flex-col items-center text-white">
+              <div className="w-12 h-12 bg-white rounded-full flex items-center justify-center p-2">
+                <RiServiceLine className="w-6 h-6 text-black" />
+              </div>
+              <p className="text-xs font-bold pt-2">Trusted Service</p>
+            </div>
           </div>
         </div>
       </div>
