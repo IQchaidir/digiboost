d6cb6c1c1a8f7aa821671afae290e756b033f9c9
Iqbal Chaidir <iqbal.chaidir@gmail.com>
2023-12-11 07:04:17 +0700

    xl 0

--- src/components/mainHero.js
+++ src/components/mainHero.js
@@ -3,34 +3,36 @@ import Image from "next/image";
 
 const MainHero = () => {
   return (
-    <div className="flex flex-col md:flex-row pt-40  mx-2 xl:mx-28 lg:mx-16 md:mx-10 sm:mx-6 justify-center mb-3">
-      <div className="text-black text-left w-full sm:w-full md:w-1/2 lg:w-1/2">
-        <div className="bg-black p-2 inline-block">
-          <h2 className="text-4xl sm:4xl md:text-5xl lg:text-5xl xl:text-6xl font-extrabold text-white">
-            BOOST...!
-          </h2>
+    <div className="mx-2 sm:mx-6  md:mx-10  lg:mx-16 xl:mx-0 ">
+      <div className="flex flex-col md:flex-row pt-40 justify-center mb-3">
+        <div className="text-black text-left w-full sm:w-full md:w-1/2 lg:w-1/2">
+          <div className="bg-black p-2 inline-block">
+            <h2 className="text-4xl sm:4xl md:text-5xl lg:text-5xl xl:text-6xl font-extrabold text-white">
+              BOOST...!
+            </h2>
+          </div>
+          <h1 className="text-4xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-6xl font-extrabold pt-4 lg:pt-8 pr-4 lg:pr-8">
+            We Help You Grow
+          </h1>
+          <h1 className="text-4xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-6xl font-extrabold pt-2 lg:pt-4 pr-4 lg:pr-8">
+            Your Business
+          </h1>
+          <p className=" text-lg sm:text-lg md:text-xl lg:text-xl xl:text-2xl  pt-4">
+            Place your trust in us, and we'll provide significant growth for
+            your business. Like other great agencies, we are committed to
+            delivering only the best results.
+          </p>
         </div>
-        <h1 className="text-4xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-6xl font-extrabold pt-4 lg:pt-8 pr-4 lg:pr-8">
-          We Help You Grow
-        </h1>
-        <h1 className="text-4xl sm:text-4xl md:text-5xl lg:text-5xl xl:text-6xl font-extrabold pt-2 lg:pt-4 pr-4 lg:pr-8">
-          Your Business
-        </h1>
-        <p className=" text-lg sm:text-lg md:text-xl lg:text-xl xl:text-2xl  pt-4">
-          Place your trust in us, and we'll provide significant growth for your
-          business. Like other great agencies, we are committed to delivering
-          only the best results.
-        </p>
-      </div>
-      <div className="flex justify-center">
-        <Image
-          src="/dm.png"
-          alt="Hero Image"
-          width={400}
-          height={400}
-          className="w-auto h-auto"
-          priority
-        />
+        <div className="flex justify-center">
+          <Image
+            src="/digital-marketing.png"
+            alt="Hero Image"
+            width={400}
+            height={400}
+            className="w-auto h-auto"
+            priority
+          />
+        </div>
       </div>
     </div>
   );
