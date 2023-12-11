5d51ed9f542bd74003538ef641423180f4066520
Iqbal Chaidir <iqbal.chaidir@gmail.com>
2023-12-11 07:19:00 +0700

    dm2

--- src/components/about.js
+++ src/components/about.js
@@ -9,6 +9,15 @@ export default function About() {
           <h1>-ABOUT-</h1>
         </div>
         <div className=" pt-20 flex flex-col lg:flex-row mx-2 sm:mx-6 xl:mx-28 lg:mx-16 md:mx-10 gap-10">
+          <div>
+            <Image
+              src="/dm2.png"
+              alt="about img"
+              width={500}
+              height={500}
+              className="h-auto w-full"
+            />
+          </div>
           <div className="text-left w-full lg:w-1/2">
             <h1 className="text-3xl text-black xl:text-5xl lg:text-3xl md:text-3xl font-bold pt-4">
               Who Is Digiboost Agency
