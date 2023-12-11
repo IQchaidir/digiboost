836ed064794d4424612c15e30407325aec7cd8e7
Iqbal Chaidir <iqbal.chaidir@gmail.com>
2023-12-12 01:53:27 +0700

    tes dekat

--- src/components/about.js
+++ src/components/about.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function About() {
   return (
-    <div className="pt-40 mb-52">
+    <div className="pt-40 mb-32">
       <div className="bg-mb-16 mx-6 sm:mx-28 xl:mx-40 lg:mx-32">
         <div className="flex flex-col fon items-center text-4xl md:text-4xl font-semibold pb-10">
           <h1>-ABOUT US-</h1>
@@ -23,7 +23,7 @@ export default function About() {
             <h1 className="text-3xl text-black xl:text-5xl lg:text-4xl md:text-4xl font-bold">
               Who Is Digiboost Agency
             </h1>
-            <p className="mx-16 lg:mx-28 text-base md:text-lg xl:text-2xl lg:text-xl pt-6 text-black">
+            <p className="mx-0 lg:mx-28 text-base md:text-lg xl:text-2xl lg:text-xl pt-6 text-black">
               Welcome to DigiBoost, your ultimate destination for innovative
               solutions to meet all your marketing and business development
               needs. At DigiBoost, we don't just provide services; we craft
